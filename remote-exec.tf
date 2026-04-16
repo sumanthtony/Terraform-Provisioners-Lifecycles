eb565e3a2e21699abb49022b15146a7a3af3048c
sumanthtony <sumanthbattagiri99@gmail.com>
2026-04-16 16:14:43 +0530

    Create remote-exec.tf

---
+++ remote-exec.tf
@@ -0,0 +1,32 @@
+resource "aws_key_pair" "mykp" {
+key_name = "sumanth-kp"
+public_key = file("/root/.ssh/id_rsa.pub")   #create a new key-pair with command: ssh-keygen and copy the public_kp here
+}
+
+resource "aws_instance" "one" {
+tags = {
+Name = "IAC-SERVER"
+}
+ami = "ami-0e12ffc2dd465f6e4"
+instance_type="t3.micro"
+count = 1
+key_name = aws_key_pair.mykp.key_name   #Attaching KEY-PAIR to the instance
+provisioner "remote-exec" {
+connection {
+  type = "ssh" 
+  user = "ec2-user" 
+  private_key = file("/root/.ssh/id_rsa")    #Connecting public_key with private key for connection
+  host = self.public_ip                      # with local-terraform server we are connecting with key-pair with SSH connection
+  }
+  
+inline = [
+"echo 'Hello World!'",
+"sudo yum install httpd -y",
+"sudo systemctl start httpd",
+"sudo mkdir sumanth",
+"sudo touch hemanth"
+]
+}
+}
+
+  
